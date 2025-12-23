9b64ee723307ffc237c2303499bfcbd8b23369be
Ahmed Alghoul <amghoul@gmail.com>
2025-12-23 16:41:13 +0200

    update

--- greet.py
+++ greet.py
@@ -1,18 +1,18 @@
 from fastmcp import FastMCP
+import logging
+import sys
+
+# Silence all logging that isn't an actual Error
+logging.getLogger("uvicorn.error").setLevel(logging.ERROR)
+logging.getLogger("uvicorn.access").setLevel(logging.ERROR)
 
 server = FastMCP("Demo")
 
 @server.tool()
 async def greet_user(name:str) -> str:
-    """
-    A tool that returns a greeting message
-    Args:
-        name: The name of the person to greet.
-    Returns:
-        str: A greeting message for the given name.
-    """
+    """A tool that returns a greeting message"""
     return f"Hello {name}, how are you today?"
 
 if __name__ == "__main__":
-    #server.run(transport="stdio")
+    # Ensure no print statements exist here
     server.run(transport="sse")
